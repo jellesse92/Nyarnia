a7a248b979df3283eb3fcbfd404deac398cad8f9
Michelle Wong <michellewong.nma@gmail.com>
2016-01-17 23:06:32 -0800

    tutorial scene

---
+++ Cat Game/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+	//public string s;
+	void Awake(){
+		
+	}
+	// Use this for initialization
+	public void ChangeToScene(string s){
+		Application.LoadLevel (s);
+	}
+}
